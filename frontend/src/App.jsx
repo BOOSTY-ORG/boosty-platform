77a36949f150519fb3b44ae72da6560b434b1c2b
seyiFortress <seyifortress@gmail.com>
2025-10-20 14:47:44 +0100

    edited the json package and comment code in client App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import RecipientOverview from './components/RecipientOverview';
-import InvestorOverview from './components/InvestorOverview';
-import Footer from './components/Footer';
+// import React, { useEffect } from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import RecipientOverview from './components/RecipientOverview';
+// import InvestorOverview from './components/InvestorOverview';
+// import Footer from './components/Footer';
 
-const App = () => {
+// const App = () => {
 
-  useEffect(() => {
-    console.log("App mounted!!!");
-    fetch('http://localhost:9000/users')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error('Error fetching data:', err));
-  }, []);
+//   useEffect(() => {
+//     console.log("App mounted!!!");
+//     fetch('http://localhost:9000/users')
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.error('Error fetching data:', err));
+//   }, []);
 
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/recipient' element={<RecipientOverview />} />
-        <Route path='/investor' element={<InvestorOverview />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
-}
+//   return (
+//     <BrowserRouter>
+//       <Header />
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/recipient' element={<RecipientOverview />} />
+//         <Route path='/investor' element={<InvestorOverview />} />
+//       </Routes>
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
