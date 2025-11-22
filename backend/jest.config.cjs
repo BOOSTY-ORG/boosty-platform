2cb48b3d4627067060c982364825aec513fd773f
seyiFortress <seyifortress@gmail.com>
2025-11-22 12:18:36 +0100

    refactor: migrate Jest and Babel configs to CommonJS format for ES modules compatibility

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,12 +1,6 @@
-export default {
+module.exports = {
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.jsx'],
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
-  },
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.cjs'],
   testMatch: [
     '<rootDir>/tests/**/*.test.js',
     '<rootDir>/src/**/*.test.js'
@@ -19,5 +13,17 @@ export default {
   coverageReporters: ['text', 'lcov', 'html'],
   transformIgnorePatterns: [
     'node_modules/(?!supertest)'
-  ]
+  ],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  preset: null,
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  globals: {
+    'babel-jest': {
+      useESM: true
+    }
+  }
 };
